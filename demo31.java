8ca262310c045547179fe3570afbfa9d03169edd
Mohammed-Asrar-U <mohammedasrar.u2023@vitstudent.ac.in>
2025-12-06 16:44:06 +0530

    Add files via upload

---
+++ demo31.java
@@ -0,0 +1,13 @@
+package demoscope;
+
+public class demo31 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a*b;
+		System.out.println("multiplication of two nummbers "+(c));
+
+	}
+
+}
